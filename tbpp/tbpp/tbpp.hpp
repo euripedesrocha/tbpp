eda63dc7f8239865cd71f1c8a029f7116465e9e4
Euripedes Rocha <euripedes@quleap.com>
2020-05-02 19:34:24 +0100

    Refactor of clocked_tb
    - Simplification of cycle
    - Makes cyle a free function called run
    - Switched to CRTP style for trace file.
    
    - This still is WIP and needs testing.

--- tbpp/tbpp/tbpp.hpp
+++ tbpp/tbpp/tbpp.hpp
@@ -1,12 +1,15 @@
 #pragma once
-#include <cstdint>
+
 #include <memory>
+#include <string>
+#include <type_traits>
 
 #include "verilated.h"
 #include "verilated_vcd_c.h"
 
 namespace tbpp {
 
+namespace detail {
 template <class Dut>
 struct stop_simulation {
   auto operator()(Dut* p) {
@@ -15,13 +18,29 @@ struct stop_simulation {
   }
 };
 
-template <bool should_trace>
+template <class Dut>
+class has_trace_file {
+ public:
+  enum { value = true };
+};
+
+template <class ClockedDut>
+auto simulation_step(ClockedDut& dut) {
+  dut->eval();
+  if constexpr (has_trace_file<ClockedDut>::value) {
+    dut.time_and_dump();
+  };
+}
+
+template <class Dut, bool should_trace>
 class trace_file {};
 
-template <>
-class trace_file<true> {
-  VerilatedVcdC* trace = nullptr;
+template <class Dut>
+class trace_file<Dut, true> {
+  VerilatedVcdC* trace;
   uint64_t sim_time = 0;
+
+  void open(char const* const file_name) { trace->open(file_name); }
 
  public:
   trace_file() : trace(new VerilatedVcdC) { Verilated::traceEverOn(true); }
@@ -30,56 +49,46 @@ class trace_file<true> {
     if (trace) trace->close();
     delete trace;
   }
-  auto get_trace() const noexcept { return trace; }
-  void open(char const* const file_name) { trace->open(file_name); }
+  auto start(std::string const file_name, std::size_t const level) {
+    static_cast<Dut>(*this)->trace(trace, level);
+    open(file_name.c_str());
+  }
   void time_and_dump() {
     ++sim_time;
     trace->dump(sim_time);
   }
 };
 
+}  // namespace detail
+
 template <class Dut>
-using base_tb = std::unique_ptr<Dut, stop_simulation<Dut>>;
+using base_tb = std::unique_ptr<Dut, detail::stop_simulation<Dut>>;
+
+template <class Dut>
+auto run(Dut& dut, std::size_t n_cycles = 1) {
+  for (std::size_t executed = 0; executed < n_cycles; ++executed) {
+    dut->clk = 0;
+    detail::simulation_step(dut);
+    dut->clk = 1;
+    detail::simulation_step(dut);
+  }
+}
 
 template <class Dut, bool should_trace = false>
-struct base_clocked_tb : trace_file<should_trace> {
-  base_tb<Dut> dut;
-
-  base_clocked_tb() : dut(new Dut){};
+class clocked_tb : public detail::trace_file<Dut, should_trace> {
+ public:
+  clocked_tb() : dut(new Dut){};
 
   auto trace_to(std::string const name, size_t const level = 10) {
-    static_assert(should_trace, "Trace parameter must be true");
-    dut->trace(this->get_trace(), level);
-    this->open(name.c_str());
+    static_assert(should_trace,
+                  "Tracing isn´t enabled. Use tbpp::clocked_tb<DUT, true>");
+    this->start(name, level);
   }
 
-  auto cycle(std::size_t n_cycles = 1) {
-    if constexpr (should_trace) {
-      cycle_with_trace(n_cycles);
-    } else {
-      cycle_no_trace(n_cycles);
-    }
-  }
+  auto operator-> () { return dut.get(); }
 
  private:
-  auto cycle_no_trace(std::size_t n_cycles = 1) {
-    for (std::size_t executed = 0; executed < n_cycles; ++executed) {
-      dut->clk = 0;
-      dut->eval();
-      dut->clk = 1;
-      dut->eval();
-    }
-  }
-  auto cycle_with_trace(std::size_t n_cycles = 1) {
-    for (std::size_t executed = 0; executed < n_cycles; ++executed) {
-      dut->clk = 0;
-      dut->eval();
-      this->time_and_dump();
-      dut->clk = 1;
-      dut->eval();
-      this->time_and_dump();
-    }
-  }
+  base_tb<Dut> dut;
 };
 
 }  // namespace tbpp
